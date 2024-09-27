f0022da39b3709f6d062a852ae9d12937c76320a
Eudes Lima <eudesfilho1994@outlook.com>
2024-09-27 14:00:16 -0300

    updated Expense Model

--- src/main/java/com/eudeslima/finance/models/Expense.java
+++ src/main/java/com/eudeslima/finance/models/Expense.java
@@ -1,8 +1,9 @@
 package com.eudeslima.finance.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 
 @Entity
@@ -10,8 +11,79 @@ import jakarta.persistence.Table;
 public class Expense {
 
     @Id
-    Long id;
-    String name;
-    String description;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(nullable = false)
+    private String name; // Nome do produto ou serviço
+
+    private String description; // Descrição opcional
+
+    @Column(nullable = false)
+    private BigDecimal totalValue; // Valor total da despesa
+
+    @Column(nullable = false)
+    private int numberOfInstallments; // Número de parcelas
+
+    @Column(nullable = false)
+    private BigDecimal installmentValue; // Valor de cada parcela
+
+    @Column(nullable = false)
+    private LocalDate startDate; // Data de início do parcelamento
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getTotalValue() {
+        return totalValue;
+    }
+
+    public void setTotalValue(BigDecimal totalValue) {
+        this.totalValue = totalValue;
+    }
+
+    public int getNumberOfInstallments() {
+        return numberOfInstallments;
+    }
+
+    public void setNumberOfInstallments(int numberOfInstallments) {
+        this.numberOfInstallments = numberOfInstallments;
+    }
+
+    public BigDecimal getInstallmentValue() {
+        return installmentValue;
+    }
+
+    public void setInstallmentValue(BigDecimal installmentValue) {
+        this.installmentValue = installmentValue;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
 }
